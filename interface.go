301021ffc706338f650a9056059d43adb7e1a182
Daisuke Maki <lestrrat+github@gmail.com>
2021-08-20 08:56:19 +0900

    Add more explicit documentation about Start()

--- interface.go
+++ interface.go
@@ -22,6 +22,16 @@ type IntervalGenerator interface {
 // implements. Users must create a controller object from this
 // policy to actually do anything with it
 type Policy interface {
+	// Start creates a new Controller object for the backoff.
+	//
+	// The Controller starts a new gouroutine that will control when
+	// the backoff events are fired. This goroutine may or may not
+	// run forever depending on your settings.
+	//
+	// Therefore the caller is expected to properly terminate the
+	// `contexts.Context` object that was provided after the
+	// desired backoff operation has been performed. Otherwise
+	// you may end up with leaked goroutines.
 	Start(context.Context) Controller
 }
 
